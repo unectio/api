a4e7d6db2cc6b498c861aa31d12f8b348dd85240
AlexeyKobets <kobets@gmail.com>
2020-03-17 21:47:03 -0700

    added collection to apilet

--- apilet/collections.go
+++ apilet/collections.go
@@ -31,13 +31,14 @@ import (
 	"github.com/unectio/util/restmux/client"
 )
 
-var Functions		= &client.Collection{"functions", nil}
-var AuthMethods		= &client.Collection{"auths", nil}
-var Repos		= &client.Collection{"repositories", nil}
-var Routers		= &client.Collection{"routers", nil}
-var Secrets		= &client.Collection{"secrets", nil}
-var Packages		= &client.Collection{"packages", nil}
+var Functions = &client.Collection{"functions", nil}
+var AuthMethods = &client.Collection{"auths", nil}
+var Repos = &client.Collection{"repositories", nil}
+var Routers = &client.Collection{"routers", nil}
+var Secrets = &client.Collection{"secrets", nil}
+var Packages = &client.Collection{"packages", nil}
 
-var FnCodes		= &client.Collection{"code", Functions}
-var FnTriggers		= &client.Collection{"triggers", Functions}
-var PkgLists		= &client.Collection{"list", Packages}
+var FnCodes = &client.Collection{"code", Functions}
+var FnTriggers = &client.Collection{"triggers", Functions}
+var PkgLists = &client.Collection{"list", Packages}
+var RepoFiles = &client.Collection{"files", Repos}
